7701bfd377ecf83c0f07626ae43480e221720446
Snafuzima <kauffman1613@gmail.com>
2021-09-07 17:43:04 +0300

    Добавлены менюшки и navigation drawer

--- app/src/main/java/com/example/notebook/MainActivity.java
+++ app/src/main/java/com/example/notebook/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.notebook;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +19,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        addNote();
+
+    }
+
+    private void addToggle() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawerLayout,
+                toolbar,
+                R.string.open_drawer,
+                R.string.close_drawer);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+
+    private void addNote() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.add_note) {
+
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.fragment_container_view, new EditNoteFragment(), "Edit")
+                            .commit();
+
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
