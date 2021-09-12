eefdc687c4968814c86e4f476599701a609dfcbf
Snafuzima <kauffman1613@gmail.com>
2021-09-12 21:47:15 +0300

    Добавлен RecyclerView

--- app/src/main/java/com/example/notebook/domain/Note.java
+++ app/src/main/java/com/example/notebook/domain/Note.java
@@ -1,21 +1,29 @@
 package com.example.notebook.domain;
 
+import android.annotation.SuppressLint;
+
 import androidx.annotation.StringRes;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Note {
 
+    private  String id;
     private  String name;
     private  String description;
     private  String date;
 
-    public Note(String name, String description, String date) {
+    public Note(String id, String name, String description) {
+        this.id=id;
         this.name=name;
         this.description=description;
-        this.date=date;
+        this.date=getDateTime();
     }
 
-    public String getName() {
-        return name;
+    public String getId() {
+        return id;
     }
 
     public String getDescription() {
@@ -24,5 +32,14 @@ public class Note {
 
     public String getDate() {
         return date;
+    }
+
+    public String getName() {return name;}
+
+    private String getDateTime() {
+        @SuppressLint("SimpleDateFormat")
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = new Date();
+        return dateFormat.format(date);
     }
 }
